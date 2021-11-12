:bug: double checked and fixed sign convention and consistency of the Moment inputs. Now they give the correct strain.

WISDEM/wisdem/rotorse/precomp.py
@@ -304,13 +304,13 @@ class PreComp:
                 xun[i] = 0.5 * (csU.loc[idx_ss] + csU.loc[idx_ss + 1])
                 yun[i] = np.interp(xun[i], pf.x, pf.yu)
 
-        # make dimensional and define relative to elastic center
+        # make dimensional and define relative to elastic center: : x positive towards TE, y positive towards Suction side
         xu = xun * self.chord - self.x_ec_nose
         xl = xln * self.chord - self.x_ec_nose
         yu = yun * self.chord - self.y_ec_nose
         yl = yln * self.chord - self.y_ec_nose
 
-        # switch to airfoil coordinate system
+        # switch to PreComp coordinate system : x positive towards Suction side, y positive towards TE
         xu, yu = yu, xu
         xl, yl = yl, xl
 

WISDEM/wisdem/rotorse/rotor_structure.py
@@ -582,8 +582,8 @@ class ProcessDels(ExplicitComponent):
         # ---> From our observations M1,M2,F3 are in th AIROIL coordinate system, NOT in the principal axes. We will thus need to rotate them by alpha.
 
         # Reinterpolate: (rotation from will be done in the next component)
-        outputs["M1"] = np.interp(s, self.s_usr, self.deMLx)
-        outputs["M2"] = np.interp(s, self.s_usr, self.deMLy)
+        outputs["M1"] = np.interp(s, self.s_usr, self.deMLx) #1: positive towards suction side
+        outputs["M2"] = np.interp(s, self.s_usr, self.deMLy) #2: positive towards trailing edge
         outputs["F3"] = np.interp(s, self.s_usr, self.deFLz)
 
 
@@ -645,8 +645,8 @@ class ProcessExtreme(ExplicitComponent):
         s = inputs["s"]
         
         # Reinterpolate: (rotation from will be done in the next component)
-        outputs["M1"] = np.interp(s, self.s_usr, self.deMLx)
-        outputs["M2"] = np.interp(s, self.s_usr, self.deMLy)
+        outputs["M1"] = np.interp(s, self.s_usr, self.deMLx) #1: positive towards suction side
+        outputs["M2"] = np.interp(s, self.s_usr, self.deMLy) #2: positive towards trailing edge
         outputs["F3"] = np.interp(s, self.s_usr, self.deFLz)
 
 
@@ -1508,7 +1508,7 @@ class RotorStructure(Group):
         self.add_subsystem("del", ProcessDels(modeling_options=modeling_options, opt_options=opt_options), promotes=["s"])
         self.add_subsystem("strains", ComputeStrains(modeling_options=modeling_options), promotes=promoteListStrains)
         self.add_subsystem("fatigue_strains", ComputeStrains(modeling_options=modeling_options, pbeam=True), promotes=promoteListStrains) 
-            #pBeam=True: DEL input are in airfoil axes. PBeam option will rotate them by alpha to put them in principal axes frame.
+            #pBeam=True: DEL input are in airfoil axes. PBeam option will swap and rotate them by alpha to put them in principal axes frame.
         self.add_subsystem("tip_pos", TipDeflection(), promotes=["tilt", "pitch_load"])
         self.add_subsystem(
             "aero_hub_loads",
@@ -1521,7 +1521,7 @@ class RotorStructure(Group):
         # if opt_options["constraints"]["blade"]["extreme_loads_from_user_inputs"]:
         self.add_subsystem("aero_gust_external", ProcessExtreme(modeling_options=modeling_options, opt_options=opt_options), promotes=["s"])
         self.add_subsystem("extreme_strains", ComputeStrains(modeling_options=modeling_options, pbeam=True), promotes=promoteListStrains) 
-            #pBeam=True: extreme input are in airfoil axes. PBeam option will rotate them by alpha to put them in principal axes frame.
+            #pBeam=True: extreme input are in airfoil axes. PBeam option will swap and rotate them by alpha to put them in principal axes frame.
 
         #Adding the constraint subsystems
         self.add_subsystem(

examples_BYU/07_test_iterateDEL/driver.py
@@ -475,24 +475,21 @@ for IGLOB in range(restartAt,nGlobalIter):
 
                         
             # c. More processing:
-            # -> switch from IEC local blade frame to "airfoil frame" with x towards TE
+            # -> switch from IEC local blade frame to "PRECOMP frame" with x positive towards TE
             #    Notes: 
-            #    - ELASTODYN output is in "local coordinate system similar to the standard blade system, 
-            #       but the  x-axis and y-axis are aligned with the local principal axes. Since this is based
-            #       on the standard convention, we have x upwards and y chordwise positive towards LE. This is
+            #    - ELASTODYN output is in "local coordinate system similar to the standard blade system", 
+            #       So we have x upwards and y chordwise positive towards LE. This is
             #       confirmed by the results that give positive M in x AND y, with moment in y (flapwise) larger.
-            #    - WISDEM expects the moments in principal axes coordinates, but in that case the 1st principal 
+            #       Note: this is NOT in principal elastic axes, just airfoil-aligned axes.
+            #    - WISDEM strain computation needs the moments in principal axes coordinates: 1st principal 
             #       direction is chordwise positive towards TE, and 2 is positive upwards. This is confirmed by 
             #       the stifness properties: EI11 is lower than EI22 (lower stifness edgewise than flapwise). 
-            #       This is also confirmed by the typical input of the strain system in WISDEM that has M1<0, M2>0
-            #       and |M1|>|M2|
-            #    CONCLUSION: I need to rotate it by 90 deg.
-            #    FURTHERMORE: ELASTODYN output is actually in AIRFOIL coordinates (not in the prinipal elastic axes),
-            #           as per verification with various results. Hence, they still need to be rotated by an angle #           alpha (this is done within wisdem).
-            tmp = DEL_life_B1[:,2].copy()
-            DEL_life_B1[:,2] = -DEL_life_B1[:,3] #the y direction in ED becomes -1 for wisdem
-            DEL_life_B1[:,3] = tmp #the x direction in ED becomes 2 for wisdem
-            DEL_life_B1[:,4] = DEL_life_B1[:,4]
+            #       However, the strain module processes the input by swapping x and y and by rotating to the 
+            #       principal axes. So out input should be in PRECOMP axes: x positive towards suction side, y positive towards TE.
+            #    CONCLUSION: switch from ED frame to PRECOMP frame by changing sign of My
+            # DEL_life_B1[:,2] = DEL_life_B1[:,2]
+            DEL_life_B1[:,3] = -DEL_life_B1[:,3] 
+            # DEL_life_B1[:,4] = DEL_life_B1[:,4]
 
             print("Damage eq loads:")
             print(np.transpose(DEL_life_B1))
@@ -661,11 +658,11 @@ for IGLOB in range(restartAt,nGlobalIter):
                 plt.show()
 
             # More processing:
-            #1) switch from IEC local blade frame to "airfoil frame" with x towards TE
-            tmp = EXTR_life_B1[:,2].copy()
-            EXTR_life_B1[:,2] = -EXTR_life_B1[:,3]
-            EXTR_life_B1[:,3] = tmp
-            EXTR_life_B1[:,4] = EXTR_life_B1[:,4]
+            #1) switch from IEC local blade frame to "PRECOMP frame" with y positive towards TE
+            # EXTR_life_B1[:,2] = EXTR_life_B1[:,2]
+            EXTR_life_B1[:,3] = -EXTR_life_B1[:,3]
+            # EXTR_life_B1[:,4] = EXTR_life_B1[:,4]
+
 
             for k in range(5):
                 EXTR_life_B1[:,k] *= fac[k]

examples_BYU/09_compare_struct/plotLoFiFailure.py
@@ -28,6 +28,11 @@ def my_write_yaml(instance, foutput):
         yaml.dump(instance, f)
 
 
+#==================== DEFAULTS  =====================================
+withEXTR = False
+withDEL = False  
+withNominal = False #REPLACE the EXTR with the nominal load
+WRONG_CONVENTION = False
 
 #==================== DEFINITIONS  =====================================
 
@@ -36,24 +41,39 @@ mydir = os.path.dirname(os.path.realpath(__file__))  # get path to this file
 fname_modeling_options = mydir + os.sep + "modeling_options.yaml"
 fname_analysis_options = mydir + os.sep + "analysis_options_struct.yaml"
 
-fname_wt_input = mydir + os.sep + "Madsen2019_10_forWEIS_isotropic.yaml"
-fname_loads = mydir + os.sep + "Madsen2019_10_forWEIS_isotropic_ED/nominalLoads.yaml" #the loading condition you want to use (should have a nominal section)
-folder_arch = mydir + os.sep + "LoFiEval_isotropic"
+# fname_wt_input = mydir + os.sep + "Madsen2019_10_forWEIS_isotropic.yaml"
+# fname_loads = mydir + os.sep + "Madsen2019_10_forWEIS_isotropic_ED/nominalLoads.yaml" #the loading condition you want to use (should have a nominal section)
+# folder_arch = mydir + os.sep + "LoFiEval_isotropic_nominalLoads"
+# withNominal = True
 
 # fname_wt_input = mydir + os.sep + "Madsen2019_10_forWEIS.yaml"
 # fname_loads = mydir + os.sep + "Madsen2019_10_forWEIS/nominalLoads.yaml" #the loading condition you want to use (should have a nominal section)
-# folder_arch = mydir + os.sep + "LoFiEval_composite"
+# folder_arch = mydir + os.sep + "LoFiEval_composite_nominalLoads"
+# withNominal = True
+
+fname_wt_input = mydir + os.sep + "Madsen2019_10_forWEIS_isotropic.yaml"
+fname_loads = mydir + os.sep + "../07_test_iterateDEL/results-IEC1.1-IEC1.3_5vels_120s_0Glob_norm_neq1/analysis_options_struct_withDEL.yaml" 
+folder_arch = mydir + os.sep + "LoFiEval_isotropic_DEL"
+withDEL = True
+WRONG_CONVENTION = True
+m_wohler = 10
+n_life_eq = 1
 
 
 runWISDEM = True
 
 
 #==================== ======== =====================================
-## Preprocessing
+## Preprocessing: filling in the loads and relevant parameters
 
-withEXTR = True  #compute EXTREME moments, well more like the strin  based on the loading you gave as an input
-withDEL = False  #leave this off: you don't need it 
+if not os.path.isdir(folder_arch):
+    runWISDEM = True
 
+if withEXTR and withDEL and withNominal:
+    raise ValueError("choose one of EXTRM, DEL or norminal")
+
+if withEXTR and withNominal:
+    raise ValueError("can't do withEXTR and withNominal: nominal loads actually replace the extreme loads")
 
 # analysis_opt = load_yaml(fname_analysis_options)
 # wt_init = load_yaml(fname_wt_input)
@@ -63,18 +83,52 @@ withDEL = False  #leave this off: you don't need it
 ## Load the loading file
 schema_loads = load_yaml(fname_loads)
 
-
 ## Update the analysis file
 schema = load_yaml(fname_analysis_options)
 
 
-schema["extreme"] = {}
-schema["extreme"]["description"] = schema_loads["nominal"]["description"]
-schema["extreme"]["grid_nd"] = schema_loads["nominal"]["grid_nd"]
-schema["extreme"]["deMLx"] = schema_loads["nominal"]["deMLx"]
-schema["extreme"]["deMLy"] = schema_loads["nominal"]["deMLy"]
-schema["extreme"]["deFLz"] = schema_loads["nominal"]["deFLz"]
-schema["constraints"]["blade"]["extreme_loads_from_user_inputs"] = True #we are using that channel as a way to specify a loading. The we will read the corresponding strain the EXTRM strain  output
+if withEXTR or withNominal:
+    if withEXTR:
+        src_name = "extreme"
+    else: 
+        src_name = "nominal"
+
+    schema["extreme"] = {}
+    schema["extreme"]["description"] = schema_loads[src_name]["description"]
+    schema["extreme"]["grid_nd"] = schema_loads[src_name]["grid_nd"]
+    if WRONG_CONVENTION:
+        #sign change was done but should not have swapped
+        schema["extreme"]["deMLx"] = ( np.array(schema_loads[src_name]["deMLy"]) ).tolist()
+        schema["extreme"]["deMLy"] = ( np.array(schema_loads[src_name]["deMLx"]) ).tolist()
+    else:
+        schema["extreme"]["deMLx"] = schema_loads[src_name]["deMLx"]
+        schema["extreme"]["deMLy"] = schema_loads[src_name]["deMLy"]
+    schema["extreme"]["deFLz"] = schema_loads[src_name]["deFLz"]
+    schema["constraints"]["blade"]["extreme_loads_from_user_inputs"] = True #we are using that channel as a way to specify a loading. The we will read the corresponding strain the EXTRM strain  output
+
+
+
+
+if withDEL:
+    schema["DEL"] = {}
+    schema["DEL"]["description"] = schema_loads["DEL"]["description"]
+    schema["DEL"]["grid_nd"] = schema_loads["DEL"]["grid_nd"]
+    if WRONG_CONVENTION:
+        #sign change was done but should not have swapped
+        schema["DEL"]["deMLx"] = ( np.array(schema_loads["DEL"]["deMLy"]) ).tolist()
+        schema["DEL"]["deMLy"] = ( np.array(schema_loads["DEL"]["deMLx"]) ).tolist()
+    else:
+        schema["DEL"]["deMLx"] = schema_loads["DEL"]["deMLx"]
+        schema["DEL"]["deMLy"] = schema_loads["DEL"]["deMLy"]
+    
+    schema["DEL"]["deFLz"] = schema_loads["DEL"]["deFLz"]
+
+    schema["constraints"]["blade"]["fatigue_spar_cap_ss"]["flag"] = True
+    schema["constraints"]["blade"]["fatigue_spar_cap_ps"]["flag"] = True
+    schema["constraints"]["blade"]["fatigue_spar_cap_ss"]["eq_Ncycle"] = float(n_life_eq)
+    schema["constraints"]["blade"]["fatigue_spar_cap_ps"]["eq_Ncycle"] = float(n_life_eq)
+    schema["constraints"]["blade"]["fatigue_spar_cap_ss"]["m_wohler"] = m_wohler
+    schema["constraints"]["blade"]["fatigue_spar_cap_ps"]["m_wohler"] = m_wohler
 
 schema["general"]["folder_output"] = folder_arch
 
@@ -85,104 +139,12 @@ my_write_yaml(schema, fname_analysis_options_struct)
 #==================== ======== =====================================
 # Simulation
 
-# overridden_values = {}
-# overridden_values["rotorse.xu_strain_spar"] = [2.60001698,2.38170205,1.62793445,0.86687718,0.69488276,1.01063347
-# ,1.26065757,1.14452006,0.97216279,0.85892484,0.76934834,0.72187777
-# ,0.67424907,0.61707346,0.5643521,0.51985643,0.48173753,0.44862879
-# ,0.41654838,0.38797813,0.36246771,0.33828892,0.31465849,0.29173286
-# ,0.27105139,0.25037732,0.22966595,0.20409592,0.16973297,0.06727553]
-# overridden_values["rotorse.xl_strain_spar"] = [-2.37821503,-2.31329415,-1.74729165,-1.02954568,-0.88685459,-1.25368234
-# ,-1.50926078,-1.35840821,-1.15849323,-1.02089267,-0.91783497,-0.85902831
-# ,-0.80425368,-0.73166315,-0.65537453,-0.59224189,-0.53825301,-0.49606488
-# ,-0.46160527,-0.42963624,-0.39932503,-0.37087766,-0.34330666,-0.31686874
-# ,-0.29312981,-0.26970749,-0.24675835,-0.21975102,-0.18601458,-0.07544804]
-# overridden_values["rotorse.yu_strain_spar"] = [0.68800779,1.02697889,0.6198455,0.21196559,0.0473643,0.10595746
-# ,0.13668742,0.02723982,-0.05129326,-0.08304079,-0.10412325,-0.10516968
-# ,-0.10115421,-0.09713379,-0.08307498,-0.08361616,-0.0810186,-0.07931845
-# ,-0.07438488,-0.07160432,-0.06875535,-0.06738904,-0.06542558,-0.06313923
-# ,-0.05940534,-0.05573268,-0.05444918,-0.06011351,-0.09258093,-0.07271351]
-# overridden_values["rotorse.yl_strain_spar"] = [-1.25674834,-1.35933239,-0.87715502,-0.51728909,-0.53327418,-0.7503961
-# ,-0.91045496,-0.86330604,-0.75638271,-0.66742097,-0.59299581,-0.53817104
-# ,-0.48307338,-0.42552989,-0.35949942,-0.31317682,-0.27125189,-0.23824789
-# ,-0.20654436,-0.18094544,-0.15863941,-0.14068945,-0.12467393,-0.11076018
-# ,-0.09766458,-0.08599894,-0.07803114,-0.07776103,-0.10558723,-0.07481666]
-
 if runWISDEM:
     wt_opt, analysis_options, opt_options = run_wisdem(fname_wt_input, fname_modeling_options, fname_analysis_options_struct,
         # overridden_values = overridden_values
     )
 
     print("\n\n\n  -------------- DONE WITH WISDEM ------------------\n\n\n\n")    
-
-#+x is ss, -x is ps
-# may get +y on ss and -y on ps, but not always
-
-#WHAT IT SHOULD BE 
-# rotorse.xu_strain_spar,"[2.60001698 2.38170205 1.62793445 0.86687718 0.69488276 1.01063347
-#  1.26065757 1.14452006 0.97216279 0.85892484 0.76934834 0.72187777
-#  0.67424907 0.61707346 0.5643521  0.51985643 0.48173753 0.44862879
-#  0.41654838 0.38797813 0.36246771 0.33828892 0.31465849 0.29173286
-#  0.27105139 0.25037732 0.22966595 0.20409592 0.16973297 0.06727553]",x-position of midpoint of spar cap on upper surface for strain calculation
-# rotorse.xl_strain_spar,"[-2.37821503 -2.31329415 -1.74729165 -1.02954568 -0.88685459 -1.25368234
-#  -1.50926078 -1.35840821 -1.15849323 -1.02089267 -0.91783497 -0.85902831
-#  -0.80425368 -0.73166315 -0.65537453 -0.59224189 -0.53825301 -0.49606488
-#  -0.46160527 -0.42963624 -0.39932503 -0.37087766 -0.34330666 -0.31686874
-#  -0.29312981 -0.26970749 -0.24675835 -0.21975102 -0.18601458 -0.07544804]",x-position of midpoint of spar cap on lower surface for strain calculation
-# rotorse.yu_strain_spar,"[ 0.68800779  1.02697889  0.6198455   0.21196559  0.0473643   0.10595746
-#   0.13668742  0.02723982 -0.05129326 -0.08304079 -0.10412325 -0.10516968
-#  -0.10115421 -0.09713379 -0.08307498 -0.08361616 -0.0810186  -0.07931845
-#  -0.07438488 -0.07160432 -0.06875535 -0.06738904 -0.06542558 -0.06313923
-#  -0.05940534 -0.05573268 -0.05444918 -0.06011351 -0.09258093 -0.07271351]",y-position of midpoint of spar cap on upper surface for strain calculation
-# rotorse.yl_strain_spar,"[-1.25674834 -1.35933239 -0.87715502 -0.51728909 -0.53327418 -0.7503961
-#  -0.91045496 -0.86330604 -0.75638271 -0.66742097 -0.59299581 -0.53817104
-#  -0.48307338 -0.42552989 -0.35949942 -0.31317682 -0.27125189 -0.23824789
-#  -0.20654436 -0.18094544 -0.15863941 -0.14068945 -0.12467393 -0.11076018
-#  -0.09766458 -0.08599894 -0.07803114 -0.07776103 -0.10558723 -0.07481666]"
-
-
-#WHAT IT IS IN THE COMPOSITE CALL:
-# rotorse.xu_strain_spar,"[-0.00237137 -0.02232077 -0.39312588 -0.59534197 -0.6264342  -0.44938689
-#  -0.22481868 -0.26423804 -0.31571896 -0.39576025 -0.36414385 -0.32376401
-#  -0.29368556 -0.27087079 -0.26695719 -0.24404249 -0.22878154 -0.20714033
-#  -0.17887004 -0.15276627 -0.1293438  -0.10864371 -0.09048269 -0.07474432
-#  -0.06135962 -0.04935499 -0.04080752 -0.03270521 -0.0227165  -0.00747272]",x-position of midpoint of spar cap on upper surface for strain calculation
-# rotorse.xl_strain_spar,"[-0.00237137 -0.02232077 -0.39312588 -0.59534197 -0.6264342  -0.44938689
-#  -0.22481868 -0.26423804 -0.31571896 -0.39576025 -0.36414385 -0.32376401
-#  -0.29368556 -0.27087079 -0.26695719 -0.24404249 -0.22878154 -0.20714033
-#  -0.17887004 -0.15276627 -0.1293438  -0.10864371 -0.09048269 -0.07474432
-#  -0.06135962 -0.04935499 -0.04080752 -0.03270521 -0.0227165  -0.00747272]",x-position of midpoint of spar cap on lower surface for strain calculation
-# rotorse.yu_strain_spar,"[-2.68994463 -2.6714235  -2.59877645 -2.59876147 -2.63808801 -2.55904653
-#  -2.44879187 -2.46592522 -2.41897788 -2.3721729  -2.33443482 -2.26927189
-#  -2.19455422 -2.12028532 -2.04105988 -1.95954551 -1.86181616 -1.76368786
-#  -1.66177813 -1.56029523 -1.46038569 -1.36359875 -1.26859831 -1.17583691
-#  -1.09181366 -1.00796706 -0.92424035 -0.86519809 -0.72733185 -0.2912122 ]",y-position of midpoint of spar cap on upper surface for strain calculation
-# rotorse.yl_strain_spar,"[-2.68994463 -2.6714235  -2.59877645 -2.59876147 -2.63808801 -2.55904653
-#  -2.44879187 -2.46592522 -2.41897788 -2.3721729  -2.33443482 -2.26927189
-#  -2.19455422 -2.12028532 -2.04105988 -1.95954551 -1.86181616 -1.76368786
-#  -1.66177813 -1.56029523 -1.46038569 -1.36359875 -1.26859831 -1.17583691
-#  -1.09181366 -1.00796706 -0.92424035 -0.86519809 -0.72733185 -0.2912122 ]",y-position of midpoint of spar cap on lower surface for strain calculation
-
-#WHAT IT IS IN THE ISOTROPIC CALL:
-# rotorse.xu_strain_spar,"[-0.00237137 -0.02232077 -0.39312588 -0.59534197 -0.6264342  -0.44938689
-#  -0.22481868 -0.26423804 -0.31571896 -0.39576025 -0.36414385 -0.32376401
-#  -0.29368556 -0.27087079 -0.26695719 -0.24404249 -0.22878154 -0.20714033
-#  -0.17887004 -0.15276627 -0.1293438  -0.10864371 -0.09048269 -0.07474432
-#  -0.06135962 -0.04935499 -0.04080752 -0.03270521 -0.0227165  -0.00747272]",x-position of midpoint of spar cap on upper surface for strain calculation
-# rotorse.xl_strain_spar,"[-0.00237137 -0.02232077 -0.39312588 -0.59534197 -0.6264342  -0.44938689
-#  -0.22481868 -0.26423804 -0.31571896 -0.39576025 -0.36414385 -0.32376401
-#  -0.29368556 -0.27087079 -0.26695719 -0.24404249 -0.22878154 -0.20714033
-#  -0.17887004 -0.15276627 -0.1293438  -0.10864371 -0.09048269 -0.07474432
-#  -0.06135962 -0.04935499 -0.04080752 -0.03270521 -0.0227165  -0.00747272]",x-position of midpoint of spar cap on lower surface for strain calculation
-# rotorse.yu_strain_spar,"[-2.68994463 -2.6714235  -2.59877645 -2.59876147 -2.63808801 -2.55904653
-#  -2.44879187 -2.46592522 -2.41897788 -2.3721729  -2.33443482 -2.26927189
-#  -2.19455422 -2.12028532 -2.04105988 -1.95954551 -1.86181616 -1.76368786
-#  -1.66177813 -1.56029523 -1.46038569 -1.36359875 -1.26859831 -1.17583691
-#  -1.09181366 -1.00796706 -0.92424035 -0.86519809 -0.72733185 -0.2912122 ]",y-position of midpoint of spar cap on upper surface for strain calculation
-# rotorse.yl_strain_spar,"[-2.68994463 -2.6714235  -2.59877645 -2.59876147 -2.63808801 -2.55904653
-#  -2.44879187 -2.46592522 -2.41897788 -2.3721729  -2.33443482 -2.26927189
-#  -2.19455422 -2.12028532 -2.04105988 -1.95954551 -1.86181616 -1.76368786
-#  -1.66177813 -1.56029523 -1.46038569 -1.36359875 -1.26859831 -1.17583691
-#  -1.09181366 -1.00796706 -0.92424035 -0.86519809 -0.72733185 -0.2912122 ]",y-position of midpoint of spar cap on lower surface for strain calculation
 
 #==================== ======== =====================================
 # Read the outputs
@@ -199,12 +161,21 @@ with np.load(WISDEMout) as a:
     r = np.array(a["rotorse.rs.z_az_m"])
     r = (r-r[0])/(r[-1]-r[0])
     
-    #My passed loading stuff:
-    data3 = a["rotorse.rs.extreme_strains.F3_N"]  #should be = to my input
-    data4 = a["rotorse.rs.extreme_strains.M1_N*m"]  #should be = to my input (well, my input but set in principal axes)
-    data5 = a["rotorse.rs.extreme_strains.M2_N*m"]  #should be = to my input (well, my input but set in principal axes)
-    data1 = a["rotorse.rs.extreme_strains.strainU_spar"] / max_strain #rebuild the failure constraint
-    data2 = a["rotorse.rs.extreme_strains.strainL_spar"] / max_strain #rebuild the failure constraint
+    if withNominal or withEXTR:
+        #My passed loading stuff:
+        data3 = a["rotorse.rs.extreme_strains.F3_N"]  #should be = to my input
+        data4 = a["rotorse.rs.extreme_strains.M1_N*m"]  #should be = to my input (well, my input but set in principal axes)
+        data5 = a["rotorse.rs.extreme_strains.M2_N*m"]  #should be = to my input (well, my input but set in principal axes)
+        data1 = a["rotorse.rs.extreme_strains.strainU_spar"] / max_strain #rebuild the failure constraint
+        data2 = a["rotorse.rs.extreme_strains.strainL_spar"] / max_strain #rebuild the failure constraint
+
+    if withDEL:
+        #My passed loading stuff:
+        data3 = a["rotorse.rs.fatigue_strains.F3_N"]  #should be = to my input
+        data4 = a["rotorse.rs.fatigue_strains.M1_N*m"]  #should be = to my input (well, my input but set in principal axes)
+        data5 = a["rotorse.rs.fatigue_strains.M2_N*m"]  #should be = to my input (well, my input but set in principal axes)
+        data1 = a["rotorse.rs.fatigue_strains.strainU_spar"] / max_strain #rebuild the failure constraint
+        data2 = a["rotorse.rs.fatigue_strains.strainL_spar"] / max_strain #rebuild the failure constraint
     
     # #original gust stuff:
     # data = a["rotorse.rs.strains.F3_N"]