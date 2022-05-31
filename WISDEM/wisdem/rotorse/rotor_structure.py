case7: multiple fixes, again in the signs of Hansen's formula, eventually explaining the minus: it comes from Hansens' frame being left-handed. Adding - where applicable to ensure positive strain is traction. For the subsequent optimization: replace the use of the previous damage-equivalent MLx,MLy,FLz by the new tilde values.

WISDEM/wisdem/rotorse/rotor_structure.py
@@ -536,9 +536,12 @@ class ProcessDels(ExplicitComponent):
         #read the DEL/DEM:
         if opt_options["constraints"]["blade"]["fatigue_spar_cap_ss"]["flag"] or opt_options["constraints"]["blade"]["fatigue_spar_cap_ps"]["flag"]:
             self.s_usr = opt_options["DEL"]["grid_nd"]
-            self.deMLx = opt_options["DEL"]["deMLx"]
-            self.deMLy = opt_options["DEL"]["deMLy"]
-            self.deFLz = opt_options["DEL"]["deFLz"]
+            # self.deMLx = opt_options["DEL"]["deMLx"]
+            # self.deMLy = opt_options["DEL"]["deMLy"]
+            # self.deFLz = opt_options["DEL"]["deFLz"]
+            self.deMLx = opt_options["DEL"]["deMLxTilde"]
+            self.deMLy = opt_options["DEL"]["deMLyTilde"]
+            self.deFLz = opt_options["DEL"]["deFLzTilde"]
         else:
             self.s_usr = -np.ones(n_span)  #not set to zero otherwise creates a divided by 0 error in evaluation of fatigue
             # self.deFn  = -np.ones(n_span)
@@ -840,9 +843,21 @@ class ComputeStrains(ExplicitComponent):
 
             # convert to principal axes, unless already there
             if self.options["pbeam"]:
-                M1, M2 = rotate(M2in, M1in)
+                M1, M2 = rotate(-M2in, -M1in)
+                # We do 2 things here:
+                # 1. We swap the x and y axes because we go from airfoil axes to Hansen's
+                # 2. Because of the swap, Hansen's axes are left-handed: a positive M1 will give positive strain in the U side, while it should be compression! 
+                #   So we add the minus sign in front of everything, making sure that positive M1 gives negative strain, that is compression.
+                #NOTE: should we be doing that without pbeam as well??
+                # -> nope, because the My corresponding to the M1,M2 below is <0 already
             else:
                 M1, M2 = M1in, M2in
+
+                # My = M1in * ca - M2in * sa
+                # Mx = M1in * sa + M2in * ca
+                # print("Gust Mx,My:")
+                # print( Mx)
+                # print( My)
 
             # compute strain
             x, y = rotate(xuu, yuu)

examples_BYU/07_test_iterateDEL/driver.py
@@ -166,6 +166,7 @@ if __name__ == '__main__':
             (-5.e3,5.e3),  #FLz [kN]
             (-6.e-3,6.e-3),  #StrainU [-]
             (-6.e-3,6.e-3),  #StrainL [-]
+            (-6.e-3,6.e-3),  #StrainTE [-]
             ]
 
 
@@ -426,7 +427,7 @@ if __name__ == '__main__':
                 fac = np.array([1.,1.,1.e3,1.e3,1.e3, 1.,1.,1.]) #multiplicator because output of AD is in N, but output of ED is in kN
                 # for the strain, Mx,My,Fz are in kN(m) but the combili factors include a 10^3, so that the output of the channel is indeed a strain
                 labs = ["Fn [N/m]","Ft [N/m]","MLx (lead lag) [Nm]","MLy (flap, +TE) [Nm]","FLz [N]","SparStrainU [-]","SparStrainL [-]","SparStrainTE [-]"]
-                labs_Lt = ["LtildeU [N]","LtildeL [N]", "MxtU [Nm]","MytU [Nm]","FztU [N]", "MxtL [Nm]","MytL [Nm]","FztL [N]"]
+                labs_Lt = ["MxtU [Nm]","MytU [Nm]","FztU [N]", "MxtL [Nm]","MytL [Nm]","FztL [N]"]
                 
                 n_processed = len(labs) #number of quantities processed
 
@@ -435,7 +436,7 @@ if __name__ == '__main__':
                 
                 # Init our lifetime DEL
                 DEL_life_B1 = np.zeros([nx,n_processed])    
-                Ltilde_life_B1 = np.zeros([nx,8])    
+                Ltilde_life_B1 = np.zeros([nx,6])    
 
                 #  -- Retreive the DELstar --
                 # (after removing "elapsed" from the del post_processing routine in weis)
@@ -666,20 +667,22 @@ if __name__ == '__main__':
                         # fact_LtU = (y2U / EI11 - x2U / EI22 + 1/EA)
                         # fact_LtL = (y2L / EI11 - x2L / EI22 + 1/EA)
 
-                        # Find the equivalent Mx,My,Fz that will give the same strain as the Damage-equivalent Life strain,
-                        #  and that also have the same ratios as DEMx,DEMy,DEFz (that is, the damage-eq loads not based on strain)
-                        Ltilde_life_B1[:,4] = DEL_life_B1[:,5] / (DEL_life_B1[:,2]/DEL_life_B1[:,4] * yoEIxxU + DEL_life_B1[:,3]/DEL_life_B1[:,4] * xoEIyyU + ooEA )
-                        Ltilde_life_B1[:,2] = DEL_life_B1[:,2]/DEL_life_B1[:,4] * Ltilde_life_B1[:,4]
-                        Ltilde_life_B1[:,3] = DEL_life_B1[:,3]/DEL_life_B1[:,4] * Ltilde_life_B1[:,4]
 
-                        Ltilde_life_B1[:,7] = DEL_life_B1[:,6] / (DEL_life_B1[:,2]/DEL_life_B1[:,4] * yoEIxxL + DEL_life_B1[:,3]/DEL_life_B1[:,4] * xoEIyyL + ooEA )
-                        Ltilde_life_B1[:,5] = DEL_life_B1[:,2]/DEL_life_B1[:,4] * Ltilde_life_B1[:,7]
-                        Ltilde_life_B1[:,6] = DEL_life_B1[:,3]/DEL_life_B1[:,4] * Ltilde_life_B1[:,7]
+                        # # d.(option1)
+                        # # Find the equivalent Mx,My,Fz that will give the same strain as the Damage-equivalent Life strain,
+                        # #  and that also have the same ratios as DEMx,DEMy,DEFz (that is, the damage-eq loads not based on strain)
+                        # Ltilde_life_B1[:,2] = DEL_life_B1[:,5] / (DEL_life_B1[:,2]/DEL_life_B1[:,4] * yoEIxxU + DEL_life_B1[:,3]/DEL_life_B1[:,4] * xoEIyyU + ooEA )
+                        # Ltilde_life_B1[:,0] = DEL_life_B1[:,2]/DEL_life_B1[:,4] * Ltilde_life_B1[:,2]
+                        # Ltilde_life_B1[:,1] = DEL_life_B1[:,3]/DEL_life_B1[:,4] * Ltilde_life_B1[:,2]
 
+                        # Ltilde_life_B1[:,5] = DEL_life_B1[:,6] / (DEL_life_B1[:,2]/DEL_life_B1[:,4] * yoEIxxL + DEL_life_B1[:,3]/DEL_life_B1[:,4] * xoEIyyL + ooEA )
+                        # Ltilde_life_B1[:,3] = DEL_life_B1[:,2]/DEL_life_B1[:,4] * Ltilde_life_B1[:,5]
+                        # Ltilde_life_B1[:,4] = DEL_life_B1[:,3]/DEL_life_B1[:,4] * Ltilde_life_B1[:,5]
 
+                        #--
+                        # # d.(option2)
                         # Find the unique equivalent Mx,My,Fz that will give the same strain as the Damage-equivalent Life strain
                         #  in the spars and at the TE simultaneously
-
                         A = np.zeros([3,3])
                         # b = np.zeros([3,1])
                         for i in range(n_span):
@@ -691,11 +694,14 @@ if __name__ == '__main__':
                             A[2,1] = xoEIyyTE[i]
                             A[:,2] = ooEA[i]
                             b = DEL_life_B1[i,5:]
-                            sol = np.linalg.solve(A, b)
+                            sol = np.linalg.solve(A, b) * 1.e3  # A assumes x vector in thousands
 
-                            Ltilde_life_B1[i,2] = sol[0]
-                            Ltilde_life_B1[i,3] = sol[1]
-                            Ltilde_life_B1[i,4] = sol[2]
+                            Ltilde_life_B1[i,0] = sol[0] 
+                            Ltilde_life_B1[i,1] = sol[1] 
+                            Ltilde_life_B1[i,2] = sol[2] 
+                        Ltilde_life_B1[:,3] = Ltilde_life_B1[:,0]
+                        Ltilde_life_B1[:,4] = Ltilde_life_B1[:,1]
+                        Ltilde_life_B1[:,5] = Ltilde_life_B1[:,2]
 
                         print("Damage eq loads:")
                         print(np.transpose(DEL_life_B1))
@@ -784,8 +790,6 @@ if __name__ == '__main__':
                             x = np.arange(rng[k][0]+dx/2.,rng[k][1],dx)
                             # normFac = 1 / (nt * dx) #normalizing factor, to bring the EXTR_distro count into non-dimensional proability
                             # EXTR_distro_B1[:,k,:] *= normFac 
-                        # EXTR_distro_B1[:,k,:] *= normFac 
-                            # EXTR_distro_B1[:,k,:] *= normFac 
                             #--> there might be missing timesteps or anything... let's just make sure the distro sum to 1.00
                             for i in range(nx):
                                 for j in range(n_aggr):
@@ -828,7 +832,7 @@ if __name__ == '__main__':
                         truncThr = None #no restriction
 
                         # new recommended setup:
-                        distr = ["norm","norm","twiceMaxForced","norm","norm","norm","norm"] 
+                        distr = ["norm","norm","twiceMaxForced","norm","norm","norm","norm","norm"] 
                         # truncThr = [0.5,1.0,None,0.5,0.5] #recommend using None if logfit=true
 
                         #TODO: check which distr is appropriate for strain?
@@ -1021,6 +1025,13 @@ if __name__ == '__main__':
                         schema["DEL"]["deFLz"] = DEL_life_B1[:,4].tolist()
                         schema["DEL"]["StrainSparU"] = DEL_life_B1[:,5].tolist()
                         schema["DEL"]["StrainSparL"] = DEL_life_B1[:,6].tolist()
+                        schema["DEL"]["StrainTE"] = DEL_life_B1[:,7].tolist()
+
+                        schema["DEL"]["deMLxTilde"] = Ltilde_life_B1[:,0].tolist()
+                        schema["DEL"]["deMLyTilde"] = Ltilde_life_B1[:,1].tolist()
+                        schema["DEL"]["deFLzTilde"] = Ltilde_life_B1[:,2].tolist()
+
+
                     if withEXTR:
                         schema["extreme"] = {}
                         schema["extreme"]["description"] = extr_descr_str
@@ -1030,6 +1041,7 @@ if __name__ == '__main__':
                         schema["extreme"]["deFLz"] = dlc["extr_loads"][0][:,4].tolist()
                         schema["extreme"]["StrainSparL"] = dlc["extr_loads"][0][:,5].tolist()
                         schema["extreme"]["StrainSparU"] = dlc["extr_loads"][0][:,6].tolist()
+                        schema["extreme"]["StrainTE"] = dlc["extr_loads"][0][:,7].tolist()
 
                     schema["general"]["folder_output"] = "outputs_struct_withFatigue"
                     if withDEL:
@@ -1078,6 +1090,23 @@ if __name__ == '__main__':
                     plt.xlabel("r/R")
                     plt.legend()
                     plt.savefig(f"{labs[k].split(' ')[0]}.png")
+
+                if withDEL:
+                    plt.subplots(nrows=1, ncols=1, figsize=(10, 5))
+                    for k in [0,1,2]:
+                        plt.plot(locs,Ltilde_life_B1[:,k] , label=labs_Lt[k])
+                        plt.ylabel("U")
+                        plt.xlabel("r/R")
+                        plt.legend()
+                        plt.savefig("LtildeU.png")
+
+                    plt.subplots(nrows=1, ncols=1, figsize=(10, 5))
+                    for k in [3,4,5]:
+                        plt.plot(locs,Ltilde_life_B1[:,k] , label=labs_Lt[k])
+                        plt.ylabel("L")
+                        plt.xlabel("r/R")
+                        plt.legend()
+                        plt.savefig("LtildeL.png")
                 if showPlots:
                     plt.show()
         elif fname_analysis_options_FORCED:

examples_BYU/09_compare_struct/plotLoFiFailure.py
@@ -32,10 +32,10 @@ def my_write_yaml(instance, foutput):
 withEXTR = False
 withDEL = False  
 withNominal = False #REPLACE the EXTR with the nominal load
-WRONG_CONVENTION = False
 importHifiCstr = ""
 HANKY_MLx = False
 HANKY_FLz = False
+USE_LTILDE = False
 
 m_wohler = 10
 n_life_eq = 1
@@ -67,17 +67,6 @@ fname_analysis_options = mydir + os.sep + "analysis_options_struct.yaml"
 # # importHifiCstr = "Madsen2019_10_forWEIS_isotropic_ED/hifiCstr_nominal.npz"
 # importHifiCstr = "Madsen2019_10_forWEIS_isotropic_ED/hifiCstr_nominalg.npz"
 # HFc = 0 # index of the corresponding constraint in HiFi
-
-
-# #Original constant thickness model, under DEL
-# fname_wt_input = mydir + os.sep + "Madsen2019_10_forWEIS_isotropic.yaml"
-# fname_loads = mydir + os.sep + "../07_test_iterateDEL/results-IEC1.1-IEC1.3_5vels_120s_0Glob_norm_neq1/analysis_options_struct_withDEL.yaml" 
-# folder_arch = mydir + os.sep + "LoFiEval_isotropic_DEL"
-# withDEL = True
-# importHifiCstr = "Madsen2019_10_forWEIS_isotropic_ED/hifiCstr_damage.npz"
-# HFc = 0 # index of the corresponding constraint in HiFi
-# WRONG_CONVENTION = True # to use old DEL files
-# HANKY_FLz = True
 
 
 
@@ -115,13 +104,8 @@ if withEXTR or withNominal:
     schema["extreme"] = {}
     schema["extreme"]["description"] = schema_loads[src_name]["description"]
     schema["extreme"]["grid_nd"] = schema_loads[src_name]["grid_nd"]
-    if WRONG_CONVENTION:
-        #sign change was done but should not have swapped
-        schema["extreme"]["deMLx"] = ( np.array(schema_loads[src_name]["deMLy"]) ).tolist()
-        schema["extreme"]["deMLy"] = ( np.array(schema_loads[src_name]["deMLx"]) ).tolist()
-    else:
-        schema["extreme"]["deMLx"] = schema_loads[src_name]["deMLx"]
-        schema["extreme"]["deMLy"] = schema_loads[src_name]["deMLy"]
+    schema["extreme"]["deMLx"] = schema_loads[src_name]["deMLx"]
+    schema["extreme"]["deMLy"] = schema_loads[src_name]["deMLy"]
     schema["extreme"]["deFLz"] = schema_loads[src_name]["deFLz"]
     schema["constraints"]["blade"]["extreme_loads_from_user_inputs"] = True #we are using that channel as a way to specify a loading. The we will read the corresponding strain the EXTRM strain  output
 
@@ -136,15 +120,14 @@ if withDEL:
     schema["DEL"] = {}
     schema["DEL"]["description"] = schema_loads["DEL"]["description"]
     schema["DEL"]["grid_nd"] = schema_loads["DEL"]["grid_nd"]
-    if WRONG_CONVENTION:
-        #sign change was done but should not have swapped
-        schema["DEL"]["deMLx"] = ( np.array(schema_loads["DEL"]["deMLy"]) ).tolist()
-        schema["DEL"]["deMLy"] = ( np.array(schema_loads["DEL"]["deMLx"]) ).tolist()
+    if USE_LTILDE:
+        schema["DEL"]["deMLx"] = schema_loads["DEL"]["deMLxTilde"]
+        schema["DEL"]["deMLy"] = schema_loads["DEL"]["deMLyTilde"]
+        schema["DEL"]["deFLz"] = schema_loads["DEL"]["deFLzTilde"]
     else:
         schema["DEL"]["deMLx"] = schema_loads["DEL"]["deMLx"]
         schema["DEL"]["deMLy"] = schema_loads["DEL"]["deMLy"]
-    
-    schema["DEL"]["deFLz"] = schema_loads["DEL"]["deFLz"]
+        schema["DEL"]["deFLz"] = schema_loads["DEL"]["deFLz"]
 
     if HANKY_MLx:
         schema["DEL"]["deMLx"] = ( np.array(schema_loads["DEL"]["deMLx"])/2. ).tolist()